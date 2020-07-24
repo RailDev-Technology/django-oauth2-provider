43a6435ed6170a013e22be8b509b37b9e491d931
root <root@dev.raildev.tech>
2020-07-24 19:08:43 +0000

    fixing asgi compat

--- provider/oauth2/middleware.py
+++ provider/oauth2/middleware.py
@@ -26,7 +26,7 @@ class Oauth2UserMiddleware(MiddlewareMixin):
     # Fixme: Not yet implemented
     def _http_access_token(self, request):
         try:
-            auth_header = request.META['HTTP_AUTHORIZATION']
+            auth_header = request.headers.get['Authorization']
             if not auth_header:
                 return None
             parts = auth_header.split()
